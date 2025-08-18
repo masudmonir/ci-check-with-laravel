17b820c42556e26b4186999dd470301f86894745
masudmonir <ahmedullahmasud820@gmail.com>
2025-08-18 09:21:46 +0600

    linter update

--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', function () {
-    return 'this is home page';
-});
-Route::get('/services', function () {
-    return 'this is services page';
-});
-Route::get('/products', function () {
-    return 'this is products page';
-});
-Route::get('/blog', function () {
-    return 'this is blog page';
-});
-Route::get('/about', function () {
-                return 'this is about page';
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/home', function () {
+        return 'this is home page';
+    });
+    Route::get('/services', function () {
+        return 'this is services page';
+    });
+    Route::get('/products', function () {
+        return 'this is products page';
+    });
+    Route::get('/blog', function () {
+        return 'this is blog page';
+    });
+    Route::get('/about', function () {
+        return 'this is about page';
+    });
