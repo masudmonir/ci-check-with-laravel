eafe60d7dcce60a91cbfc8556474b9eb8d24bbb5
masudmonir <ahmedullahmasud820@gmail.com>
2025-08-20 07:30:54 +0600

    composer require laravel/breeze --dev and php artisan breeze:install

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,21 +13,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    Route::get('/home', function () {
-        return 'this is home page from web.php';
-    });
-    Route::get('/services', function () {
-        return 'this is services page from web.php';
-    });
-    Route::get('/products', function () {
-        return 'this is products page';
-    });
-    Route::get('/blog', function () {
-        return 'this is blog page';
-    });
-    Route::get('/about', function () {
-        return 'this is about page';
-    });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
